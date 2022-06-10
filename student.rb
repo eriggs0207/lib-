dc67d333d7deb84cba21e730aab833c8bc8b7ca9
Erik Riggs <riggs.erik@gmail.com>
2022-06-10 15:41:31 -0600

    Use of rspec to test code

---
+++ student.rb
@@ -0,0 +1,21 @@
+require 'respec'
+require './lib/student.rb'
+
+describe Student do
+  it 'is an instance of a student' do
+    student1 = Student.new('Erik')
+    expect(student1).to be_a Student
+
+  end
+
+  it 'has a name' do
+    student1 = Student.new('Erik')
+    expect(student1.name).to eq "Erik"
+  end
+
+it 'is enrolled in school' do
+  student1 = Student.new('Erik')
+expect(student1.enrollment).to eq true
+
+end
+end
